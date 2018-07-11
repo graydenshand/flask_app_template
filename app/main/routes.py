61679562973ae48e0413b944eebc7a8a29b1e4ec
Grayden Shand <grayden@Graydens-MacBook-Pro.local>
2018-07-11 15:55:39 -0400

    New Repo

---
+++ app/main/routes.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from flask import render_template, flash, redirect, url_for, request, g, \
+    jsonify, current_app
+from flask_login import current_user, login_required
+from app import db
+from app.models import User
+from app.main import bp
+
+
+@bp.route('/', methods=['GET', 'POST'])
+@bp.route('/index', methods=['GET', 'POST'])
+@login_required
+def index():
+    return render_template('index.html', title='Home')
+
+
+@bp.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html', user=user)
